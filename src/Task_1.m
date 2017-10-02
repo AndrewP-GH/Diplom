6d434fac526f5df6abb029840f9e304f7a80daae
Andrew Paramonov <paravoz.z@yandex.ru>
2017-10-03 00:35:02 +0400

    Paper incorrect

--- src/Task_1.m
+++ src/Task_1.m
@@ -19,12 +19,11 @@ g_max = g_max/m;
 val_g = 0;              %начальное управление
 g = @(t, val) 0;        %управл€юща€ функци€
 
-f = NewFigure('ѕр€ма€ задача');
-saver =  CSaveAsGif();
+NewFigure('ѕр€ма€ задача');
 
 W = 0.2*sin(pi*X);	    %начальное распределение точек листа
 V = zeros(1,Nx);        %начальное распределение скорости листа
-U = -pi^2*0.2*sin(pi*X);
+U = pi*0.2*cos(pi*X);
 
 P = [ W; V; U];
 P(:,1) = 0;             %граничное левое
@@ -36,22 +35,22 @@ Beta = zeros(3,1,Nx);
 nu = dt/(dx^2);
 A = [ 
         0 0 0; 
-        nu*(V0^2-c^2) -V0*dt/dx D/m*nu; 
+        0 -V0*dt/dx 0; 
         1 0 0 
     ];
 B = [ 
         1 -dt 0; 
-        -2*nu*(V0^2-c^2) 1 -2*D/m*nu; 
+        0 1 (V0^2-c^2)*dt; 
         -2 0 -dx^2 
     ];
 C = [ 
         0 0 0; 
-        nu*(V0^2-c^2) V0*dt/dx D/m*nu; 
+        0 V0*dt/dx 0; 
         1 0 0 
     ];
 
 hold all;
-axis([0 Lx 0 0.2]);
+%axis([0 Lx 0 0.2]);
 grid on;
 plot(0,0, 'w');
 p1 = plot(X,P(1,:));
@@ -61,7 +60,6 @@ set (p1, 'Xdata', X, 'Ydata', P(1,:));
 drawnow;  
 for t=0:dt:T-dt
     F = [ P(1,:); P(2,:) + g(t+dt,val_g)*dt; zeros(1,Nx) ];
-    Alpha(:,:,2) = [ 0 0 0; 0 0 0; 0 2/(dx^2) 0];
     for i=3:Nx
         Alpha(:,:,i) = -(A*Alpha(:,:,i-1)+B)\C;
         Beta(:,:,i) = (A*Alpha(:,:,i-1)+B)\(F(:,i-1)-A*Beta(:,:,i-1));
@@ -78,35 +76,33 @@ end
 f = NewFigure('ќбратна€ задача');
 figure(f);
 %axis([0 Lx -2.5 1.5]);
-q1 = P(3,:)*(T0+Ro*V0^2);
+q1 = zeros(1,Nx);
 q2 = zeros(1,Nx);
-% for i = 2:Nx-1
-%     q1(i) = q1(i) + Ro*V0*(P(2,i+1)-P(2,i-1))/(2*dx);
-%     q2(i) = -Ro*(P(2,i)+V0*(P(1,i+1)-P(1,i-1))/(2*dx));
-% end
 for i = 2:Nx-1
-    q1(i) = q1(1) + Ro*V0*P(2,i)/dx;
+    q1(i) = q1(i) + Ro*V0*(P(2,i+1)-P(2,i-1))/(2*dx);
     q2(i) = -Ro*(P(2,i)+V0*(P(1,i+1)-P(1,i-1))/(2*dx));
 end
-Q = [q1; q2; zeros(1,Nx)];
+q1 = P(2,:) .* P(2,:);
+% for i = 2:Nx-1
+%     q1(i) = q1(1) + Ro*V0*P(2,i);
+%     q2(i) = -Ro*(P(2,i)+V0*(P(1,i+1)-P(1,i-1))/(2*dx));
+% end
+Q = [q1; q2];
 Q(:,1) = 0;
 Q(:,Nx) = 0;
 dt = -dt;
 nu = dt/(dx^2);
 A = [ 
-        0 nu*(c^2-V0^2) -nu*D/m; 
-        0 -nu*dx*V0 0;                      
-        0 1 0 
+        0 nu*(c^2-V0^2); 
+        0 -dt/dx*V0
     ];
 B = [ 
-        1 -2*nu*(c^2-V0^2) 2*nu*D/m; 
-        dt 1 0; 
-        0 -2 -dx^2 
+        1 -2*nu*(c^2-V0^2); 
+        dt 1 
     ];
 C = [ 
-        0 nu*(c^2-V0^2) -nu*D/m; 
-        0 nu*dx*V0 0;                       
-        0 1 0 
+        0 nu*(c^2-V0^2); 
+        0 dt/dx*V0
     ];
 grid on;
 hold all;
@@ -115,35 +111,10 @@ p5 = plot(X,Q(1,:));
 p6 = plot(X,Q(2,:));
 legend(['t=' num2str(T,tFormatStr)],'q1','q2');
 drawnow;
-pause(5);
-tmpCoef = [ 
-        0 0 -nu*D/m; 
-        0 0 0;                       
-        0 0 0 
-    ];
-Alpha = zeros(3,3,Nx);
-Beta = zeros(3,1,Nx);
-m = ceil(Nx/2);
+Alpha = zeros(2,2,Nx);
+Beta = zeros(2,1,Nx);
 for t=T:dt:0-dt
-    F = [ Q(1,:); Q(2,:); zeros(1,Nx) ];
-%     Alpha(:,:,2) = -tmpCoef;
-%     for i=3:m-1
-%         Alpha(:,:,i) = -(A*Alpha(:,:,i-1)+B)\C;
-%         Beta(:,:,i) = (A*Alpha(:,:,i-1)+B)\(F(:,i-1)-A*Beta(:,:,i-1));
-%     end
-%     Alpha(:,:,Nx-1) = -tmpCoef\A;
-%     for i=Nx-2:-1:m
-%         Alpha(:,:,i) = -(A*Alpha(:,:,i+1)+B)\C;
-%         Beta(:,:,i) = (A*Alpha(:,:,i+1)+B)\(F(:,i)-A*Beta(:,:,i+1));
-%     end
-%     Q(:,m-1) = (1-Alpha(:,:,m)*Alpha(:,:,m))\(Alpha(:,:,m)*Beta(:,:,m)+Beta(:,:,m));
-%     Q(:,m) = (1-Alpha(:,:,m)*Alpha(:,:,m))\(Alpha(:,:,m)*Beta(:,:,m)+Beta(:,:,m))*Alpha(:,:,m)+Beta(:,:,m);
-%     for i=m-2:-1:2
-%         Q(:,i) = Alpha(:,:,i+1)*Q(:,i+1)+Beta(:,:,i+1);
-%     end
-%     for i=m+1:Nx-1
-%         Q(:,i) = Alpha(:,:,i)*Q(:,i-1)+Beta(:,:,i);
-%     end
+    F = [ Q(1,:); Q(2,:)];
     for i=3:Nx
         Alpha(:,:,i) = -(A*Alpha(:,:,i-1)+B)\C;
         Beta(:,:,i) = (A*Alpha(:,:,i-1)+B)\(F(:,i-1)-A*Beta(:,:,i-1));
@@ -154,7 +125,7 @@ for t=T:dt:0-dt
     set (p5, 'Xdata', X, 'Ydata', Q(1,:));
     set (p6, 'Xdata', X, 'Ydata', Q(2,:));
     legend(['t=' num2str(t+dt,tFormatStr)],'q1','q2');
-    pause(1);
     drawnow;
+    pause(1);
 end
 
